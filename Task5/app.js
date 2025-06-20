aa39eaa99978912c6c1bb78972074a1ada0fef62
soumen0818 <sdas721444@gmail.com>
2025-06-20 22:07:22 +0530

    Task5

---
+++ Task5/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) =>{
+    res.send ("hey");
+})
+
+
+app.listen(3000);
